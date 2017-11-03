a24fed31aa4129ef9676b6ea40431e163a067802
Sergiy Pugach <sergiypugach@kh0lm1000000001.sats.corp>
2017-11-03 11:30:43 +0530

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Sergiy Pugach on 02/11/17.
+//  Copyright © 2017 Sergiy Pugach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
